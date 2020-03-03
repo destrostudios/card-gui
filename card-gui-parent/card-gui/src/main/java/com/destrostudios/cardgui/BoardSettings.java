10a6319878176fd4d64c50f1db67d66206dd1a64
carlkuesters <carl.kuesters@youengineering.com>
2020-03-04 00:36:25 +0100

    Fix settings default values.

--- card-gui-parent/card-gui/src/main/java/com/destrostudios/cardgui/BoardSettings.java
+++ card-gui-parent/card-gui/src/main/java/com/destrostudios/cardgui/BoardSettings.java
@@ -9,11 +9,17 @@ import lombok.*;
 @Getter
 public class BoardSettings {
 
+    @Builder.Default
     private String inputActionPrefix = "cardgui";
+    @Builder.Default
     private float draggedCardProjectionZ = 0.8f;
+    @Builder.Default
     private boolean draggedCardTiltEnabled = true;
+    @Builder.Default
     private float draggedCardTiltUpdateInterval = (1f / 60);
+    @Builder.Default
     private float draggedCardTiltMaximumCursorSpeed = 600;
+    @Builder.Default
     private float draggedCardTiltMaximumAngle = FastMath.QUARTER_PI;
 
 }

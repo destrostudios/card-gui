a13b37979a89694ed89985e35eb843451e1daffb
carlkuesters <carl.kuesters@youengineering.com>
2020-07-09 03:09:57 +0200

    Improved performance of the testapps by introducing an image cache.

--- card-gui-parent/card-gui-test/src/main/java/com/destrostudios/cardgui/test/files/FileAssets.java
+++ card-gui-parent/card-gui-test/src/main/java/com/destrostudios/cardgui/test/files/FileAssets.java
@@ -4,68 +4,51 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.util.HashMap;
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 
 /**
  *
  * @author Carl
  */
 public class FileAssets {
-    
+
     public static String ROOT;
-    
+    private static HashMap<String, BufferedImage> IMAGES = new HashMap<>();
+
     public static void readRootFile() {
         ROOT = FileManager.getFileContent("./assets.ini");
     }
-    
-    public static boolean exists(String filePath) {
-        return FileManager.existsFile(ROOT + filePath);
-    }
-    
-    public static ImageIcon getImageIcon(String filePath) {
-        return new ImageIcon(ROOT + filePath);
-    }
-    
-    public static ImageIcon getImageIcon(String filePath, int width, int height) {
-        return new ImageIcon(getImage(filePath, width, height));
-    }
-    
+
     public static BufferedImage getImage(String filePath, int width, int height) {
-        BufferedImage image = getImage(filePath);
-        if (width == -1) {
-            width = (int) ((((float) image.getWidth()) / image.getHeight()) * height);
-        } else if (height == -1) {
-            height = (int) ((((float) image.getHeight()) / image.getWidth()) * width);
-        }
-        if ((width != image.getWidth()) || (height != image.getHeight())) {
-            BufferedImage resizedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-            Graphics2D graphics = resizedImage.createGraphics();
-            graphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-            graphics.drawImage(image, 0, 0, width, height, 0, 0, image.getWidth(), image.getHeight(), null);
-            graphics.dispose();
-            return resizedImage;
-        }
-        return image;
+        String key = filePath + "_" + width + "_" + height;
+        return IMAGES.computeIfAbsent(key, k -> {
+            BufferedImage image = getImage(filePath);
+            int finalWidth = width;
+            int finalHeight = height;
+            if (finalWidth == -1) {
+                finalWidth = (int) ((((float) image.getWidth()) / image.getHeight()) * finalHeight);
+            } else if (finalHeight == -1) {
+                finalHeight = (int) ((((float) image.getHeight()) / image.getWidth()) * finalWidth);
+            }
+            if ((finalWidth != image.getWidth()) || (finalHeight != image.getHeight())) {
+                BufferedImage resizedImage = new BufferedImage(finalWidth, finalHeight, BufferedImage.TYPE_INT_ARGB);
+                Graphics2D graphics = resizedImage.createGraphics();
+                graphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+                graphics.drawImage(image, 0, 0, finalWidth, finalHeight, 0, 0, image.getWidth(), image.getHeight(), null);
+                graphics.dispose();
+                return resizedImage;
+            }
+            return image;
+        });
     }
-    
+
     public static BufferedImage getImage(String filePath) {
         try {
             return ImageIO.read(new File(ROOT + filePath));
         } catch (Exception ex) {
             System.err.println("Error while reading image file '" + filePath + "'.");
             ex.printStackTrace();
-        }
-        return null;
-    }
-    
-    public static FileInputStream getInputStream(String filePath) {
-        try {
-            return new FileInputStream(ROOT + filePath);
-        } catch (FileNotFoundException ex) {
-            System.err.println("Error while reading file '" + filePath + "'.");
         }
         return null;
     }

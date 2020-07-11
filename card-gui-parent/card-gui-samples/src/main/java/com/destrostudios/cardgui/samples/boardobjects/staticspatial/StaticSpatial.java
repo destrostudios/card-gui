8036dc34b0c7f9604c99d60007220e503a1f711a
carlkuesters <carl.kuesters@youengineering.com>
2020-07-11 15:03:32 +0200

    Add followTarget to StaticSpatial.

--- card-gui-parent/card-gui-samples/src/main/java/com/destrostudios/cardgui/samples/boardobjects/staticspatial/StaticSpatial.java
+++ card-gui-parent/card-gui-samples/src/main/java/com/destrostudios/cardgui/samples/boardobjects/staticspatial/StaticSpatial.java
@@ -11,4 +11,15 @@ public class StaticSpatial extends TransformedBoardObject<StaticSpatialModel> {
     public StaticSpatial() {
         super(new StaticSpatialModel());
     }
+
+    @Override
+    public void update(float lastTimePerFrame) {
+        super.update(lastTimePerFrame);
+        TransformedBoardObject followTarget = getModel().getFollowTarget();
+        if (followTarget != null) {
+            position().setCurrentValue(followTarget.position().getCurrentValue());
+            rotation().setCurrentValue(followTarget.rotation().getCurrentValue());
+            scale().setCurrentValue(followTarget.scale().getCurrentValue());
+        }
+    }
 }
--- card-gui-parent/card-gui-samples/src/main/java/com/destrostudios/cardgui/samples/boardobjects/staticspatial/StaticSpatialModel.java
+++ card-gui-parent/card-gui-samples/src/main/java/com/destrostudios/cardgui/samples/boardobjects/staticspatial/StaticSpatialModel.java
@@ -1,11 +1,13 @@
 package com.destrostudios.cardgui.samples.boardobjects.staticspatial;
 
 import com.destrostudios.cardgui.BoardObjectModel;
+import com.destrostudios.cardgui.TransformedBoardObject;
 import com.jme3.scene.Spatial;
 
 public class StaticSpatialModel extends BoardObjectModel {
 
     private Spatial spatial;
+    private TransformedBoardObject followTarget;
 
     public Spatial getSpatial() {
         return spatial;
@@ -13,5 +15,13 @@ public class StaticSpatialModel extends BoardObjectModel {
 
     public void setSpatial(Spatial spatial) {
         updateIfNotEquals(this.spatial, spatial, () -> this.spatial = spatial);
+    }
+
+    public TransformedBoardObject getFollowTarget() {
+        return followTarget;
+    }
+
+    public void setFollowTarget(TransformedBoardObject followTarget) {
+        updateIfNotEquals(this.followTarget, followTarget, () -> this.followTarget = followTarget);
     }
 }
